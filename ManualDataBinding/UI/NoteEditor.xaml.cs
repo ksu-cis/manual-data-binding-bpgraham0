b5df39ba5b3f2cdd7184db1b9bf073aedbcc283a
Benjamin Graham <grahamfamily15@hotmail.com>
2020-03-24 20:27:18 -0500

    2 way data binding

--- ManualDataBinding/UI/NoteEditor.xaml.cs
+++ ManualDataBinding/UI/NoteEditor.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿    using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -22,6 +22,57 @@ namespace ManualDataBinding.UI
         public NoteEditor()
         {
             InitializeComponent();
+        }
+
+        private Note note;
+        /// <summary>
+        /// The note that will be editedS
+        /// </summary>
+        public Note Note
+        {
+            get { return note; }
+            set 
+            {
+                if (note != null) note.NoteChanged -= OnNoteChanged;
+                note = value;
+                if (note != null)
+                {
+                    note.NoteChanged += OnNoteChanged;
+                    OnNoteChanged(note, new EventArgs());
+                }
+            }
+        }
+
+        /// <summary>
+        /// event handler that occurs when the note is updated 
+        /// </summary>
+        /// <param name="sender">note is changed</param>
+        /// <param name="e">event arguments </param>
+        public void OnNoteChanged(object sender, EventArgs e)
+        {
+            if (Note == null) return;
+            Title.Text = Note.Title;
+            Body.Text = Note.Body;
+        }
+
+        /// <summary>
+        /// Notifies when editor title gets changed and changes the note to fit
+        /// </summary>
+        /// <param name="sender">change in text</param>
+        /// <param name="e">event arguments</param>
+        public void OnTitleChanged(object sender, TextChangedEventArgs e)
+        {
+            Note.Title = Title.Text;
+
+        }
+        /// <summary>
+        /// Notifies when editor body gets changed and changes the note to fit
+        /// </summary>
+        /// <param name="sender">change in text</param>
+        /// <param name="e">event arguments</param>
+        public void OnBodyChanged(object sender, TextChangedEventArgs e)
+        {
+            Note.Body = Body.Text;
         }
     }
 }
